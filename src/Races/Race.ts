564ca537bd62624e8f3067eb3534b0a6c2f76c19
Ludson96 <ludson_ps27@hotmail.com>
2023-01-19 20:55:25 -0300

    fix: altera atributos readonly]

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,5 +1,8 @@
 export default abstract class Race {
-  constructor(private _name: string, private _dexterity: number) {}
+  constructor(
+    private readonly _name: string, 
+    private readonly _dexterity: number,
+  ) {}
 
   public get name(): string {
     return this._name;
