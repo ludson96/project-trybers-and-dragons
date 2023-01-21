3f306df966eaa8723de3c4ce22c210357da2df61
Ludson96 <ludson_ps27@hotmail.com>
2023-01-21 18:56:11 -0300

    fix: index objects

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import Battle, { PVP, PVE } from './Battle';
 import Character from './Character';
-import Dragon from './Dragon';
 import Monster from './Monster';
+import Dragon from './Dragon';
+import PVP from './Battle/PVP';
+import PVE from './Battle/PVE';
+import Battle from './Battle/Battle';
 
-const player1 = new Character('Joel');
-const player2 = new Character('Ellie');
-const player3 = new Character('Buckley');
-
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
+const player1 = new Character('P1');
+const player2 = new Character('P2');
+const player3 = new Character('P3');
 player1.levelUp();
 player1.levelUp();
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
+
 const pvp = new PVP(player2, player3);
+
 const pve = new PVE(player1, [monster1, monster2]);
 
 const runBattles = (battles: Battle[]): void => {
-  battles.forEach((e) => e.fight());
+  battles.forEach((battle) => battle.fight());
 };
 
 export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
