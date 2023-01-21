67c2363e6c4c5a2ed0eb6a112f49daff4ee46ab6
Ludson96 <ludson_ps27@hotmail.com>
2023-01-20 21:33:44 -0300

    feat: create initial setup class Character

---
+++ src/Character.ts
@@ -0,0 +1,32 @@
+import Fighter from './Fighter';
+import Race from './Races/index';
+import Archetype from './Archetypes/index';
+import Energy from './Energy';
+import getRandomInt from './utils';
+import Elf from './Races/Elf';
+import Mage from './Archetypes/Mage';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType, 
+      amount: getRandomInt(1, 10),
+    };
+  }
+}
