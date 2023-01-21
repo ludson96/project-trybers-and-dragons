f9c873868235aeb5556b55d7506d06e60c14e248
Ludson96 <ludson_ps27@hotmail.com>
2023-01-21 17:15:53 -0300

    feat: create class PVE

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,24 @@
+import Fighter from '../Fighter/index';
+import SimpleFighter from '../Fighter/SimpleFighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(
+    private _p1: Fighter,
+    private _monster: Fighter[] | SimpleFighter[],
+  ) {
+    super(_p1);
+  }
+
+  fight(): number {
+    this._monster.forEach((m) => {
+      if (this._p1.lifePoints > 0 || m.lifePoints > 0) {
+        this._p1.attack(m);
+        m.attack(this._p1);
+      }
+      console.log(this._p1.lifePoints);
+    });
+    
+    return super.fight();
+  }
+}
